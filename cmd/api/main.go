d77fb2974102f95b72a3ad30d1a65c65ddabdb34
Mohamed Abdelazim <mabdelazemahmed@gmail.com>
2025-10-15 21:45:33 +0300

    Implement environment variable handling for configuration

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"log"
+
+	"github.com/moabdelazem/feed/internal/env"
 )
 
 func main() {
 	cfg := config{
-		addr: ":3000",
+		addr: env.GetString("ADDR", ":8000"),
 	}
 
 	app := &application{
 		config: cfg,
 	}
 
-	log.Fatal(app.run(app.mount()))
+	mux := app.mount()
+
+	log.Fatal(app.run(mux))
 }
